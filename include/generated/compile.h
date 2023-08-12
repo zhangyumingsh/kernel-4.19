65ea0c289a1984a9559c8b3175731105c5762108
zhangyumingsh <492964829@qq.com>
2023-08-12 00:30:34 -0400

    origin add

---
+++ include/generated/compile.h
@@ -0,0 +1,10 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP Tue Jun 27 11:46:23 CST 2023"
+#define LINUX_COMPILE_BY "armbian"
+#define LINUX_COMPILE_HOST "next"
+#define LINUX_BRANCH "kernel-station-p2-4.19"
+#define LINUX_COMMIT "45894f6c410a224efaac6a4f4438c5768821f11b"
+#define XML_NAME "sdk version:"
+#define LINUX_COMPILER "gcc version 11.3.0 (Ubuntu 11.3.0-1ubuntu1~22.04.1), GNU ld (GNU Binutils for Ubuntu) 2.38"
